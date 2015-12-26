ae04c17c08c033bb68bc335a99045cd0854b51e7
Naoki Miyata <naokeeper.plus@gmail.com>
2015-12-27 05:29:32 +0900

    use templates and custom directives

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,45 +1,71 @@
 (function() {
   var myApp = angular.module('dashboardApp', []);
 
-  myApp.controller('graphCtrl', function($scope) {
-    $scope.graphs = [
-      {
-        'label': 'first',
-        'description': '1st desc'
-      }, {
-        'label': 'second',
-        'description': '2nd desc'
-      }, {
-        'label': 'third',
-        'description': '3rd desc'
-      }, {
-        'label': 'fourth',
-        'description': '4th desc'
-      }
-    ];
+  myApp.directive('dashboardNavi', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'dashboard_navi'
+    };
   });
 
-  myApp.controller('tableCtrl', function($scope) {
-    $scope.items = [
-      {
-        'number': 1001,
-        'first': 'Lorem',
-        'second': 'ipsum',
-        'third': 'dolor',
-        'fourth': 'sit',
-      }, {
-        'number': 1002,
-        'first': 'amet',
-        'second': 'consectetur',
-        'third': 'adipiscing',
-        'fourth': 'elit',
-      }, {
-        'number': 1003,
-        'first': 'Integer',
-        'second': 'nec',
-        'third': 'odio',
-        'fourth': 'Praesent',
-      }
-    ];
+  myApp.directive('dashboardSidebar', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'dashboard_sidebar'
+    };
+  });
+
+  myApp.directive('dashboardGraphs', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'dashboard_graphs',
+      controller: function() {
+        this.graphs = [
+          {
+            'label': 'first',
+            'description': '1st desc'
+          }, {
+            'label': 'second',
+            'description': '2nd desc'
+          }, {
+            'label': 'third',
+            'description': '3rd desc'
+          }, {
+            'label': 'fourth',
+            'description': '4th desc'
+          }
+        ];
+      },
+      controllerAs: 'graphs'
+    };
+  });
+
+  myApp.directive('dashboardTable', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'dashboard_table',
+      controller: function() {
+        this.items = [{
+            'number': 1001,
+            'first': 'Lorem',
+            'second': 'ipsum',
+            'third': 'dolor',
+            'fourth': 'sit',
+          }, {
+            'number': 1002,
+            'first': 'amet',
+            'second': 'consectetur',
+            'third': 'adipiscing',
+            'fourth': 'elit',
+          }, {
+            'number': 1003,
+            'first': 'Integer',
+            'second': 'nec',
+            'third': 'odio',
+            'fourth': 'Praesent',
+          }];
+      },
+      controllerAs: 'table'
+    };
   });
 })();
