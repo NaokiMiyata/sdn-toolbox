1998dc391608bfc8df0b001ceeefd4201f43aba9
Naoki Miyata <naokeeper.plus@gmail.com>
2015-12-27 06:02:42 +0900

    use $http service to get data from the server

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -4,45 +4,37 @@
   myApp.directive('dashboardContent', function() {
     return {
       restrict: 'E',
-      templateUrl: 'dashboard_content'
+      templateUrl: 'templates/dashboard_content'
     };
   });
 
   myApp.directive('dashboardNavi', function() {
     return {
       restrict: 'E',
-      templateUrl: 'dashboard_navi'
+      templateUrl: 'templates/dashboard_navi'
     };
   });
 
   myApp.directive('dashboardSidebar', function() {
     return {
       restrict: 'E',
-      templateUrl: 'dashboard_sidebar'
+      templateUrl: 'templates/dashboard_sidebar'
     };
   });
 
   myApp.directive('dashboardGraphs', function() {
     return {
       restrict: 'E',
-      templateUrl: 'dashboard_graphs',
-      controller: function() {
-        this.graphs = [
-          {
-            'label': 'first',
-            'description': '1st desc'
-          }, {
-            'label': 'second',
-            'description': '2nd desc'
-          }, {
-            'label': 'third',
-            'description': '3rd desc'
-          }, {
-            'label': 'fourth',
-            'description': '4th desc'
-          }
-        ];
-      },
+      templateUrl: 'templates/dashboard_graphs',
+      controller: ['$http', function($http) {
+        var graphs = this;
+
+        graphs.graphs = [];
+
+        $http.get('graphs.json').success(function(data) {
+          graphs.graphs = data;
+        });
+      }],
       controllerAs: 'graphs'
     };
   });
@@ -50,28 +42,16 @@
   myApp.directive('dashboardTable', function() {
     return {
       restrict: 'E',
-      templateUrl: 'dashboard_table',
-      controller: function() {
-        this.items = [{
-            'number': 1001,
-            'first': 'Lorem',
-            'second': 'ipsum',
-            'third': 'dolor',
-            'fourth': 'sit',
-          }, {
-            'number': 1002,
-            'first': 'amet',
-            'second': 'consectetur',
-            'third': 'adipiscing',
-            'fourth': 'elit',
-          }, {
-            'number': 1003,
-            'first': 'Integer',
-            'second': 'nec',
-            'third': 'odio',
-            'fourth': 'Praesent',
-          }];
-      },
+      templateUrl: 'templates/dashboard_table',
+      controller: ['$http', function($http) {
+        var table = this;
+
+        table.items = [];
+
+        $http.get('table.json').success(function(data) {
+          table.items = data;
+        });
+      }],
       controllerAs: 'table'
     };
   });
