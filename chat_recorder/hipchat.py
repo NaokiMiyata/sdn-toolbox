edca1a2177638ebe227e6ff99ada3f61e263dcd6
Naoki Miyata <naoki.miyata@ntt.com>
2016-01-23 11:16:37 +0900

    指定日のログを取ってくる

--- chat_recorder/hipchat.py
+++ chat_recorder/hipchat.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+import pprint
+
 filename = 'hipchat.json'
 
 config = None
@@ -8,13 +10,25 @@ config = None
 with open(filename) as f:
     config = json.load(f)
 
+def payload_day(year, month, day):
+    iso_8601 = '%4d%02d%02dT000000+0900'
+    return {
+        'end-date': iso_8601 % (year, month, day),
+        'date': iso_8601 % (year, month, day+1),
+    }
+
 url = 'https://api.hipchat.com/v2/user/%s/history' % config['user']
 payload = {
-    'auth_token': config['token']
+    'auth_token': config['token'],
 }
+
+payload.update(payload_day(2016, 1, 20))
+pprint.pprint(payload)
 
 response = requests.get(url, params=payload)
 history = response.json()
 
+pprint.pprint(history)
 
-
+for item in history['items']:
+    print item['message'].encode('utf-8')
